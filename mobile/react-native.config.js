5f738953b7a0229baac3cebbc3d880548a0c0887
mshoffmann <franiahoffmann@gmail.com>
2022-07-26 02:15:47 +0100

    nova branch

---
+++ mobile/react-native.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: ['./src/assets/fonts/'],
+};
+
+// Google: custom font on react native
---
+++ mobile/src/views/Login/index.tsx
@@ -0,0 +1,6 @@
+import {Text} from 'react-native';
+import React from 'react';
+
+export function LoginView() {
+  return <Text>aqui vai ser o login</Text>;
+}
